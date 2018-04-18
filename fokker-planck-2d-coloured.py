dbba3d887930c0f276189f5233ad855877b25a80
Urbain Vaes <urbain@vaes.uk>
2018-04-18 17:40:11 +0100

    Encapsulate load from cache in function

--- fokker-planck-2d-coloured.py
+++ fokker-planck-2d-coloured.py
@@ -143,108 +143,53 @@ disc_asymptotic_sol = quad_visu_x.discretize(asymptotic_sol/norm_asymptotic_sol)
 # }}}
 # SPECTRAL METHOD FOR STATIONARY EQUATION {{{
 
-if cache:
+def compute_with_cache(cache):
+
+    def call_discretize():
+        m_mat =  quad_num.discretize_op(m_operator, f, degree, 2)
+        r_mat =  quad_num.discretize_op(r_operator, f, degree, 2)
+        return m_mat, r_mat
+
+    args_m = [m_operator, f, degree, 2]
+    args_r = [r_operator, f, degree, 2]
+
+    hash_config = hash(frozenset(args_m + args_r))
+
     try:
-        m_mat_operator = np.load('cache/m_mat-' + str(hash_config) + '.npy')
-        r_mat_operator = np.load('cache/r_mat-' + str(hash_config) + '.npy')
+        m_mat_cache = np.load('cache/m_mat-' + str(hash_config) + '.npy')
+        r_mat_cache = np.load('cache/r_mat-' + str(hash_config) + '.npy')
+
     except IOError:
-        m_mat_operator =  quad_num.discretize_op(m_operator, f, degree, 2)
-        r_mat_operator =  quad_num.discretize_op(r_operator, f, degree, 2)
-        np.save('cache/m_mat-' + str(hash_config), m_mat_operator)
-        np.save('cache/r_mat-' + str(hash_config), r_mat_operator)
-else:
-    m_mat_operator =  quad_num.discretize_op(m_operator, f, degree, 2)
-    r_mat_operator =  quad_num.discretize_op(r_operator, f, degree, 2)
+        m_mat, r_mat = call_discretize()
+        np.save('cache/m_mat-' + str(hash_config), m_mat)
+        np.save('cache/r_mat-' + str(hash_config), r_mat)
+        return m_mat, r_mat
 
-try:
-    m_mat_operator_cache = np.load('cache/m_mat-' + str(hash_config) + '.npy')
-    r_mat_operator_cache = np.load('cache/r_mat-' + str(hash_config) + '.npy')
-    assert la.norm(m_mat_operator - m_mat_operator_cache, 2) < 1e-10
-    assert la.norm(r_mat_operator - r_mat_operator_cache, 2) < 1e-10
-except IOError:
-    pass
+    if not cache:
+        m_mat, r_mat = call_discretize()
+        assert la.norm(m_mat - m_mat_cache, 2) < 1e-10
+        assert la.norm(r_mat - r_mat_cache, 2) < 1e-10
 
-m_values = np.linspace(-1, 1, 11)
+    return m_mat, r_mat
 
-# m_num = 0
+m_mat, r_mat = compute_with_cache(glob['cache'])
+
+# m_values = np.linspace(-1, 1, 11)
+
+
+m_num = 0
 # for i in range(10):
-#     total_mat = r_mat_operator + m_num * m_mat_operator
+#     total_mat = r_mat + m_num * m_mat
 
 
-# Calculate eigenvector in kernel
+# Calculate eigenvalues of largest real part
 print("Solving the eigenvalue problem...")
-mat_operator = r_mat_operator
+mat_operator = r_mat
 sparse_operator = scipy.sparse.csr_matrix(mat_operator)
 asymptotic_sol_2d = sym.exp(- params['βx'] * functions['Vp']
                             - params['βy'] * functions['Vy'])
 v0 = quad_num.transform(asymptotic_sol_2d/factor, degree, norm=True).coeffs
 eigen_values, eigen_vectors = las.eigs(mat_operator, v0=v0, k=4, which='LR', ncv=10)
-print(eigen_values)
-
-degrees = np.arange(degree + 1)
-
-# Plot of the eigenvectors
-fig, ((ax11, ax12), (ax21, ax22)) = plt.subplots(2, 2)
-
-axes = (ax11, ax12, ax21, ax22)
-for e_val, e_vec, ax in zip(eigen_values, eigen_vectors.T, axes):
-    e_vec = np.real(e_vec) * np.sign(np.real(e_vec[0]))
-    series = quad_num.series(e_vec, norm=True)
-    cont = quad_visu_xy.plot(series, degree, factor, ax)
-    ax.set_title("Eigenvalue: " + str(e_val))
-    plt.colorbar(cont, ax=ax)
-
-plt.show()
-
-    # series_xy = hm.Series(solution_xy, mean=mean_xy, cov=cov_xy, norm=True)
-    # series_x = hm.Series(solution_x, mean=[numerics['μx']], cov=[[numerics['σx']]], norm=True)
-    # series_y = hm.Series(solution_y, mean=[0], cov=[[cov_y]], norm=True)
-
-    # solution_visu_xy = quad_visu_xy.eval(series_xy, degree)*factor_visu_xy
-    # solution_visu_xy = solution_visu_xy.reshape(nv, nv).T
-    # solution_visu_x = quad_visu_x.eval(series_x, degree) * factor_visu_x
-    # solution_visu_y = quad_visu_y.eval(series_y, degree) * factor_visu_y
-
-    # cont = plt.contourf(x_visu, y_visu, solution_visu_xy, 100)
-    # plt.title("Eigenvalue: " + str(eigen_value) + ", Minimum: " + str(np.min(solution_visu_xy)))
-    # plt.colorbar(cont)
-    # plt.show()
-
-fig, ((ax11, ax12), (ax21, ax22)) = plt.subplots(2, 2)
-
-ax11.plot(x_visu, solution_visu_x, label="Coloured noise")
-ax11.plot(x_visu, disc_asymptotic_sol, label="Asymptotic solution")
-ax11.set_title("Comparison of the solutions")
-ax11.legend()
-
-ax12.bar(degrees, series_x.coeffs)
-ax12.set_title("Hermite coefficients of numerical solution")
-
-ax21.bar(degrees, series_x.coeffs - series_asymptotic_sol.coeffs)
-ax21.set_title("Difference between Hermite coefficients")
-
-cont = ax22.contourf(x_visu, y_visu, solution_visu_xy, 100)
-ax22.set_title("SM eigenvalues: " + str(eigen_values))
-
-plt.colorbar(cont)
-
-if x_visu[0] < x_min:
-    ax11.axvline(x=x_min)
-    ax22.axvline(x=x_min)
-
-if x_visu[-1] > x_max:
-    ax11.axvline(x=x_max)
-    ax22.axvline(x=x_max)
-
-if y_visu[0] < y_min:
-    ax22.axhline(y=y_min)
-    ax21.axvline(x=y_min)
-
-if y_visu[-1] > y_max:
-    ax21.axvline(x=y_max)
-    ax22.axhline(y=y_max)
-
-plt.show()
 
 ## PLOTS {{{
 
