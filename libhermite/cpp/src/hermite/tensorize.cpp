490b3aba5ce0503d0a8f0578a4c27e2c96048c69
Urbain Vaes <urbain@vaes.uk>
2018-04-30 11:58:00 +0100

    Generalize tensorizing of matrices

--- libhermite/cpp/src/hermite/tensorize.cpp
+++ libhermite/cpp/src/hermite/tensorize.cpp
@@ -1,4 +1,5 @@
 #include <boost/math/special_functions/binomial.hpp>
+#include <iostream>
 
 #include "hermite/iterators.hpp"
 #include "hermite/tensorize.hpp"
@@ -29,10 +30,11 @@ vec tensorize_vecs(mat input)
     vec results(n_polys, 0.);
 
     Multi_index_iterator m(dim, degree);
-    for (u_int i = 0; !m.isFull(); i++, m.increment())
+    u_int i,j;
+    for (i = 0; !m.isFull(); i++, m.increment())
     {
         results[i] = 1;
-        for (u_int j = 0; j < dim; j++)
+        for (j = 0; j < dim; j++)
         {
             results[i] *= input[j][m[j]];
         }
@@ -40,7 +42,6 @@ vec tensorize_vecs(mat input)
     return results;
 }
 
-// TODO: Tensorize two vectors (urbain, Sun 29 Apr 2018 09:18:08 PM BST)
 vec tensorize_vec(vec input, u_int dim, u_int dir)
 {
     u_int degree = input.size() - 1;
@@ -50,26 +51,42 @@ vec tensorize_vec(vec input, u_int dim, u_int dir)
     return tensorize_vecs(vecs);
 }
 
-mat tensorize_mat(mat input, u_int dim, u_int dir)
+mat tensorize_mats(cube input)
 {
-    u_int degree = input.size() - 1;
+    u_int dim = input.size();
+    u_int degree = input[0].size() - 1;
     u_int n_polys = (u_int) binomial_coefficient<double> (degree + dim, dim);
     mat results(n_polys, vec(n_polys, 0.));
 
     Multi_index_iterator m1(dim, degree);
     Multi_index_iterator m2(dim, degree);
-    u_int i,j;
+    u_int i,j,k;
     for (i = 0, m1.reset(); !m1.isFull(); i++, m1.increment())
     {
         for (j = 0, m2.reset(); !m2.isFull(); j++, m2.increment())
         {
-            if (isAligned(m2.get() - m1.get(), dir))
+            results[i][j] = 1.;
+            for (k = 0; k < dim; k++)
             {
-                results[i][j] = input[m1[dir]][m2[dir]];
+                results[i][j] *= input[k][m1[k]][m2[k]];
             }
         }
     }
     return results;
+}
+
+mat tensorize_mat(mat input, u_int dim, u_int dir)
+{
+    u_int degree = input.size() - 1;
+    vec zeros(degree + 1, 0.);
+    mat eye(degree + 1, zeros);
+    for (u_int i = 0; i < degree + 1; ++i)
+    {
+        eye[i][i] = 1.;
+    }
+    cube mats(dim, eye);
+    mats[dir] = input;
+    return tensorize_mats(mats);
 }
 
 vec project_vec(vec input, u_int dim, u_int dir)
