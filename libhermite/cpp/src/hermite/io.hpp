83782b5e704998f6c965ad6b97240e95b416f1e1
Urbain Vaes <urbain@vaes.uk>
2018-05-03 13:25:45 +0100

    Use templates in io.hpp

--- libhermite/cpp/src/hermite/io.hpp
+++ libhermite/cpp/src/hermite/io.hpp
@@ -3,13 +3,43 @@
 
 #include "types.hpp"
 #include <string>
+#include <iostream>
 
 namespace hermite {
+using namespace std;
 
-void printVec(std::vec);
-void printMat(std::mat, std::string s = "  ");
-void printCube(std::cube, std::string s = " ");
-
+template<typename T> void printVec(vector<T> a)
+{
+    cout << "[" << a[0];
+    for (u_int i = 1; i < a.size(); i++)
+    {
+            cout << ", " << a[i];
+    }
+    cout << "]";
 }
+
+template<typename T> void printMat(vector< vector<T> >  a, string begin)
+{
+    cout << "["; printVec<T>(a[0]);
+    for (u_int i = 1; i < a.size(); i++)
+    {
+        cout << endl << begin;
+        printVec<T>(a[i]);
+    }
+    cout << "]";
+}
+
+template<typename T> void printCube(vector< vector< vector<T> >  > a, string begin)
+{
+    cout << "[";
+    printMat<T>(a[0]);
+    for (u_int i = 1; i < a.size(); i++)
+    {
+        cout << endl << begin;
+        printMat<T>(a[i]);
+    }
+    cout << "]" << endl;
+}}
+
 
 #endif
