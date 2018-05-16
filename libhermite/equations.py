711370d39d27d2ea6fb549741ea1023457f3ea41
Urbain Vaes <urbain@vaes.uk>
2018-05-16 18:32:30 +0100

    Generalize solve gaussian to dimension 1

--- libhermite/equations.py
+++ libhermite/equations.py
@@ -1,4 +1,5 @@
 import sympy as sym
+import sympy.printing as syp
 sym.init_printing()
 
 
@@ -6,19 +7,32 @@ def map_operator(operator, function, factor):
     return (operator.subs(function, function*factor).doit()/factor).simplify()
 
 
-def solve_gaussian_2d(operator, fxy, variables):
-    x, y = variables
-    cx, cxy, cy = sym.symbols('sol_cx sol_cxy sol_cy', real=True)
-    ansatz = sym.exp(-sym.Rational(1, 2)*(cx*x*x + 2*cxy*x*y + cy*y*y))
-    res = (operator.subs(fxy, ansatz)/ansatz).doit().cancel()
-    coeffs_dict = res.as_poly(x, y).as_dict()
-    coeffs_list = list(coeffs_dict[k] for k in coeffs_dict)
-    (sol_cx, sol_cxy, sol_cy) = sym.solve(coeffs_list, cx, cxy, cy)[0]
-    solution = ansatz.subs([(cx, sol_cx), (cxy, sol_cxy), (cy, sol_cy)])
-    assert operator.subs(fxy, solution).doit().expand() == 0
-    determinant = sol_cx * sol_cy - sol_cxy * sol_cxy
-    factor = sym.sqrt(determinant) / (2*sym.pi)
-    solution = factor * solution
+def solve_gaussian(operator, f, variables):
+
+    if len(variables) is 2:
+        x, y = variables
+        cx, cxy, cy = sym.symbols('sol_cx sol_cxy sol_cy', real=True)
+        ansatz = sym.exp(-sym.Rational(1, 2)*(cx*x*x + 2*cxy*x*y + cy*y*y))
+        res = (operator.subs(f, ansatz)/ansatz).doit().cancel()
+        coeffs_dict = res.as_poly(x, y).as_dict()
+        coeffs_list = list(coeffs_dict[k] for k in coeffs_dict)
+        (sol_cx, sol_cxy, sol_cy) = sym.solve(coeffs_list, cx, cxy, cy)[0]
+        solution = ansatz.subs([(cx, sol_cx), (cxy, sol_cxy), (cy, sol_cy)])
+        determinant = sol_cx * sol_cy - sol_cxy * sol_cxy
+        factor = sym.sqrt(determinant) / (2*sym.pi)
+        solution = factor * solution
+
+    if len(variables) is 1:
+        x = variables[0]
+        s2 = sym.symbols('s2', real=True, positive=True)
+        ansatz = 1/sym.sqrt(2*sym.pi*s2)*sym.exp(-sym.Rational(1, 2)*(x*x/s2))
+        res = (operator.subs(f, ansatz)/ansatz).doit().cancel()
+        coeffs_dict = res.as_poly(x).as_dict()
+        coeffs_list = list(coeffs_dict[k] for k in coeffs_dict)
+        sol_s2 = sym.solve(coeffs_list, s2)[s2]
+        solution = ansatz.subs(s2, sol_s2)
+
+    assert operator.subs(f, solution).doit().expand() == 0
     return solution
 
 
@@ -32,9 +46,6 @@ class McKean_Vlasov:
 
     # Unknown
     f = sym.Function('f')(variables[0], variables[1])
-
-    def test():
-        print("Hey!")
 
     @classmethod
     def params(cls):
@@ -78,10 +89,10 @@ class McKean_Vlasov:
 class Fokker_Planck_1d:
 
     # Space variables
-    variables = tuple(sym.symbols('x', real=True))
+    variables = sym.symbols('x', real=True)
 
     # Sharthand notations
-    x = variables[0]
+    x = variables
 
     # Unknown
     f = sym.Function('f')(x)
@@ -97,7 +108,7 @@ class Fokker_Planck_1d:
         β, Vp = (params[x] for x in ['β', 'Vp'])
 
         # Shorthand notations
-        d, x, f = sym.diff, cls.x, cls.y, cls.f
+        d, x, f = sym.diff, cls.x, cls.f
 
         # Fokker planck operator
         return d(d(Vp, x)*f + 1/β * d(f, x), x)
