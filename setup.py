22f6020b55fca60936273c2100847c53bf93d6bf
Urbain Vaes <urbain@vaes.uk>
2018-05-19 18:48:03 +0100

    Make setup.py minimal

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-
 from distutils.command.build_ext import build_ext as build_ext_orig
 import os
-import pathlib
-
-import pdb
 
 
 class build_ext(build_ext_orig):
@@ -14,23 +10,18 @@ class build_ext(build_ext_orig):
     def run(self):
         for ext in self.extensions:
             self.build_cmake(ext)
-        super().run()
 
     def build_cmake(self, ext):
         cwd = os.getcwd()
-        build_temp = pathlib.Path(self.build_temp)
-        build_temp.mkdir(parents=True, exist_ok=True)
-        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
-        extdir.mkdir(parents=True, exist_ok=True)
+        os.makedirs(self.build_temp, exist_ok=True)
+        os.chdir(self.build_temp)
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY='
-                      + str(extdir.parent.absolute())]
-
-        os.chdir(str(build_temp))
+                      + cwd + '/' + self.build_lib]
         self.spawn(['cmake', cwd + '/cpp'] + cmake_args)
-        # pdb.set_trace()
         if not self.dry_run:
             self.spawn(['make', '-j4'])
         os.chdir(str(cwd))
+
 
 setup(name='Hermite',
       version='v0.1',
