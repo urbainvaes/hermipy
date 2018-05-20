ca5d8955c7ff5ddf17cdca8b667ac790a3220d67
Urbain Vaes <urbain@vaes.uk>
2018-05-20 18:53:18 +0100

    Split tests

---
+++ tests/test_equations.py
@@ -0,0 +1,316 @@
+import os
+import unittest
+import sympy as sym
+import numpy as np
+import numpy.linalg as la
+import scipy.sparse.linalg as las
+import matplotlib.pyplot as plt
+
+import hermite.quad as hm
+import hermite.equations as eq
+import hermite.settings as rc
+
+settings = {'cache': False, 'cachedir': '/tmp/test_hermite'}
+rc.settings.update(settings)
+if not os.path.exists(settings['cachedir']):
+    os.makedirs(settings['cachedir'])
+
+
+class TestConvergenceFokkerPlanck1d(unittest.TestCase):
+
+    def setUp(self):
+
+        # Equation parameters
+        equation = eq.Fokker_Planck_1d
+        self.x, self.f = equation.x, equation.f
+
+        # Inverse temperature
+        self.β = sym.Rational(3)
+
+        # Common parameters of numerical approximation
+        self.degree = 100
+        self.n_points_num = 2*self.degree + 1
+
+    def sym_calc(self, Vp, m, s2):
+
+        # Equation parameters
+        equation = eq.Fokker_Planck_1d
+        x, f = equation.x, equation.f
+
+        # Calculation of the solution
+        new_q = hm.Quad.gauss_hermite
+
+        quad = new_q(self.n_points_num, dim=1, mean=[m], cov=[[s2]])
+
+        # Potential for approximation
+        Vq = sym.Rational(1/2)*(x-m)*(x-m)/(self.β*s2)
+
+        # Fokker Plarck operator
+        forward = equation.equation({'β': self.β, 'Vp': Vp})
+
+        # Map to appropriate space
+        factor = sym.exp(- self.β / 2 * (Vq + Vp))
+
+        # Mapped operator
+        backward = eq.map_operator(forward, f, factor)
+
+        # Discretize factor
+        factor = quad.discretize(factor)
+
+        return quad, forward, backward, factor
+
+    def solve(self, backward, quad, factor, degrees):
+
+        # Discretization of the operator
+        mat = quad.discretize_op(backward, self.f, self.degree, 2)
+
+        solutions = []
+        for d in degrees:
+            sub_mat = (mat[0:d+1, 0:d+1]).copy(order='C')
+            eig_vals, eig_vecs = las.eigs(sub_mat, k=1, which='LR')
+            ground_state = np.real(eig_vecs.T[0])
+            ground_state = ground_state * np.sign(ground_state[0])
+            ground_state_eval = quad.eval(quad.series(ground_state))*factor
+            norm = quad.norm(ground_state_eval, n=1, l2=True)
+            ground_state_eval = ground_state_eval / norm
+            solutions.append(ground_state_eval)
+        return solutions
+
+    def test_exact_sol_gaussian(self):
+
+        Vp, m, s2 = self.x*self.x/4, sym.Rational(1, 10), sym.Rational(1, 5)
+        quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
+        solution = eq.solve_gaussian(forward, self.f, [self.x])
+        norm_sol = quad.integrate(solution, l2=True)
+        self.assertTrue(abs(norm_sol - 1) < 1e-6)
+
+    def test_gaussian(self):
+
+        Vp, m, s2 = self.x*self.x/4, sym.Rational(1, 10), sym.Rational(1, 5)
+        quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
+
+        # Exact Solution
+        solution = eq.solve_gaussian(forward, self.f, [self.x])
+        solution_eval = quad.discretize(solution)
+
+        # Numerical solutions
+        degrees = list(range(5, self.degree))
+        solutions = self.solve(backward, quad, factor, degrees)
+
+        # Associated errors
+        errors = []
+        for sol in solutions:
+            error = quad.norm(sol - solution_eval, l2=True)
+            errors.append(error)
+
+        log_errors = np.log(errors)
+        poly_approx = np.polyfit(degrees, log_errors, 1)
+        errors_approx = np.exp(np.polyval(poly_approx, degrees))
+        error = la.norm(log_errors - np.log(errors_approx), 2)
+
+        plt.semilogy(degrees, errors, 'k.')
+        plt.semilogy(degrees, errors_approx)
+        # plt.show()
+
+        self.assertTrue(errors[-1] < 1e-12)
+        self.assertTrue(error < 5)
+
+    def test_norm_solution_bistable(self):
+
+        # Parameters of potential associated with Hermite polynomials
+        Vp = self.x**4/4 - self.x**2/2
+        m, s2 = sym.Rational(1/10), sym.Rational(1/10)
+        quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
+
+        sol_an = sym.exp(-self.β * Vp)
+        sol_num = quad.discretize(sol_an)
+        norm_1_an = quad.norm(sol_an, n=1, l2=True)
+        norm_1_num = quad.norm(sol_num, n=1, l2=True)
+        norm_2_an = quad.norm(sol_an, n=2, l2=True)
+        norm_2_num = quad.norm(sol_num, n=2, l2=True)
+        self.assertAlmostEqual(norm_1_an, norm_1_num)
+        self.assertAlmostEqual(norm_2_an, norm_2_num)
+
+    def test_bistable(self):
+
+        # Parameters of potential associated with Hermite polynomials
+        Vp = self.x**4/4 - self.x**2/2
+        m, s2 = sym.Rational(1/10), sym.Rational(1/10)
+        quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
+
+        # Exact Solution
+        exact_sol = sym.exp(-self.β * Vp)
+        norm = quad.norm(exact_sol, n=1, l2=True)
+        exact_sol = exact_sol / norm
+        solution_eval = quad.discretize(exact_sol)
+
+        degrees = list(range(10, self.degree))
+        solutions = self.solve(backward, quad, factor, degrees)
+
+        # Associated errors
+        errors = []
+        for sol in solutions:
+            error = quad.norm(sol - solution_eval, l2=True)
+            errors.append(error)
+
+        log_errors = np.log(errors)
+        poly_approx = np.polyfit(degrees, log_errors, 1)
+        errors_approx = np.exp(np.polyval(poly_approx, degrees))
+        error = la.norm(log_errors - np.log(errors_approx), 2)
+
+        plt.semilogy(degrees, errors, 'k.')
+        plt.semilogy(degrees, errors_approx)
+        # plt.show()
+
+        self.assertTrue(errors[-1] < 1e-10)
+        self.assertTrue(error < 10)
+
+
+# class TestConvergenceFokkerPlanck2d(unittest.TestCase):
+
+#     def setUp(self):
+
+#         # Equation parameters
+#         equation = eq.McKean_Vlasov
+#         self.x, self.y, self.f = equation.x, equation.y, equation.f
+
+#         # Inverse temperature
+#         self.β = sym.Rational(3)
+
+#         # Common parameters of numerical approximation
+#         self.degree = 50
+#         self.n_points_num = 2*self.degree + 1
+
+#     def sym_calc(self, Vp, m, s2):
+
+#         # Equation parameters
+#         equation = eq.McKean_Vlasov
+#         x, y, f = equation.x, equation.y, equation.f
+
+#         # Calculation of the solution
+#         new_q = hm.Quad.gauss_hermite
+
+#         quad = new_q(self.n_points_num, dim=1, mean=[m], cov=[[s2]])
+
+#         # Potential for approximation
+#         Vq = sym.Rational(1/2)*(x-m)*(x-m)/(self.β*s2)
+
+#         # Fokker Plarck operator
+#         forward = equation.equation({'β': self.β, 'Vp': Vp})
+
+#         # Map to appropriate space
+#         factor = sym.exp(- self.β / 2 * (Vq + Vp))
+
+#         # Mapped operator
+#         backward = eq.map_operator(forward, f, factor)
+
+#         # Discretize factor
+#         factor = quad.discretize(factor)
+
+#         return quad, forward, backward, factor
+
+#     def solve(self, backward, quad, factor, degrees):
+
+#         # Discretization of the operator
+#         mat = quad.discretize_op(backward, self.f, self.degree, 2)
+
+#         solutions = []
+#         for d in degrees:
+#             sub_mat = (mat[0:d+1, 0:d+1]).copy(order='C')
+#             eig_vals, eig_vecs = las.eigs(sub_mat, k=1, which='LR')
+#             ground_state = np.real(eig_vecs.T[0])
+#             ground_state = ground_state * np.sign(ground_state[0])
+#             ground_state_eval = quad.eval(quad.series(ground_state))*factor
+#             norm = quad.norm(ground_state_eval, n=1, l2=True)
+#             ground_state_eval = ground_state_eval / norm
+#             solutions.append(ground_state_eval)
+#         return solutions
+
+#     def test_exact_sol_gaussian(self):
+
+#         Vp, m, s2 = self.x*self.x/4, sym.Rational(1, 10), sym.Rational(1, 5)
+#         quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
+#         solution = eq.solve_gaussian(forward, self.f, [self.x])
+#         norm_sol = quad.integrate(solution, l2=True)
+#         self.assertTrue(abs(norm_sol - 1) < 1e-6)
+
+#     def test_gaussian(self):
+
+#         Vp, m, s2 = self.x*self.x/4, sym.Rational(1, 10), sym.Rational(1, 5)
+#         quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
+
+#         # Exact Solution
+#         solution = eq.solve_gaussian(forward, self.f, [self.x])
+#         solution_eval = quad.discretize(solution)
+
+#         # Numerical solutions
+#         degrees = list(range(5, self.degree))
+#         solutions = self.solve(backward, quad, factor, degrees)
+
+#         # Associated errors
+#         errors = []
+#         for sol in solutions:
+#             error = quad.norm(sol - solution_eval, l2=True)
+#             errors.append(error)
+
+#         log_errors = np.log(errors)
+#         poly_approx = np.polyfit(degrees, log_errors, 1)
+#         errors_approx = np.exp(np.polyval(poly_approx, degrees))
+#         error = la.norm(log_errors - np.log(errors_approx), 2)
+
+#         plt.semilogy(degrees, errors, 'k.')
+#         plt.semilogy(degrees, errors_approx)
+#         # plt.show()
+
+#         self.assertTrue(errors[-1] < 1e-12)
+#         self.assertTrue(error < 5)
+
+#     def test_norm_solution_bistable(self):
+
+#         # Parameters of potential associated with Hermite polynomials
+#         Vp = self.x**4/4 - self.x**2/2
+#         m, s2 = sym.Rational(1/10), sym.Rational(1/10)
+#         quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
+
+#         sol_an = sym.exp(-self.β * Vp)
+#         sol_num = quad.discretize(sol_an)
+#         norm_1_an = quad.norm(sol_an, n=1, l2=True)
+#         norm_1_num = quad.norm(sol_num, n=1, l2=True)
+#         norm_2_an = quad.norm(sol_an, n=2, l2=True)
+#         norm_2_num = quad.norm(sol_num, n=2, l2=True)
+#         self.assertAlmostEqual(norm_1_an, norm_1_num)
+#         self.assertAlmostEqual(norm_2_an, norm_2_num)
+
+#     def test_bistable(self):
+
+#         # Parameters of potential associated with Hermite polynomials
+#         Vp = self.x**4/4 - self.x**2/2
+#         m, s2 = sym.Rational(1/10), sym.Rational(1/10)
+#         quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
+
+#         # Exact Solution
+#         exact_sol = sym.exp(-self.β * Vp)
+#         norm = quad.norm(exact_sol, n=1, l2=True)
+#         exact_sol = exact_sol / norm
+#         solution_eval = quad.discretize(exact_sol)
+
+#         degrees = list(range(10, self.degree))
+#         solutions = self.solve(backward, quad, factor, degrees)
+
+#         # Associated errors
+#         errors = []
+#         for sol in solutions:
+#             error = quad.norm(sol - solution_eval, l2=True)
+#             errors.append(error)
+
+#         log_errors = np.log(errors)
+#         poly_approx = np.polyfit(degrees, log_errors, 1)
+#         errors_approx = np.exp(np.polyval(poly_approx, degrees))
+#         error = la.norm(log_errors - np.log(errors_approx), 2)
+
+#         plt.semilogy(degrees, errors, 'k.')
+#         plt.semilogy(degrees, errors_approx)
+#         # plt.show()
+
+#         self.assertTrue(errors[-1] < 1e-10)
+#         self.assertTrue(error < 10)
--- tests/test_hermite.py
+++ tests/test_hermite.py
@@ -1,18 +1,15 @@
 import hermite.core as core
-import hermite.hermite as hm
+import hermite.quad as hm
 import hermite.settings as rc
-import hermite.equations as eq
 
 import unittest
 import numpy as np
 import numpy.polynomial.hermite_e as herm
 import numpy.linalg as la
 import sympy as sym
-import scipy.sparse.linalg as las
 import math
 import os
 import tempfile
-import matplotlib.pyplot as plt
 
 settings = {'cache': False, 'cachedir': '/tmp/test_hermite'}
 rc.settings.update(settings)
@@ -312,20 +309,20 @@ class TestTensorizeDecorator(unittest.TestCase):
                 cov_ij = self.quad.integrate(fun)
                 self.assertAlmostEqual(cov_ij, self.cov[i][j])
 
-    def testSpVarf5dSimple(self):
+    def test_sparse_varf_5d_simple(self):
         degree = 12
         function = 1
         sp_var = self.quad.varf(function, degree, sparse=True)
         self.assertTrue(sp_var.nnz == sp_var.shape[0])
 
-    def testSpVarf5dComposite(self):
+    def test_sparse_varf_5d(self):
         degree = 10
         function = 1. + self.v[0] + self.v[1] + self.v[1]*self.v[0] \
             + self.v[2]**2 + self.v[3]**3 + self.v[4]
         sp_var = self.quad.varf(function, degree, sparse=True)
         self.assertTrue(sp_var.nnz < sp_var.shape[0] * sp_var.shape[1])
 
-    def testConsistency(self):
+    def test_consistency(self):
         degree = 3
         function = 1. + self.v[0] + self.v[1] + self.v[1]*self.v[0] \
             + self.v[2]**2 + self.v[3]**3 + self.v[4]
@@ -347,13 +344,13 @@ class TestSparseFunctions(unittest.TestCase):
     def tearDown(self):
         rc.settings.update(settings)
 
-    def testSpVarfSimple(self):
+    def test_sparse_varf_simple(self):
         degree = 30
         function = '1'
         sp_var = self.quad2.varf(function, degree, sparse=True)
         self.assertEqual(sp_var.nnz, sp_var.shape[0])
 
-    def testSpVarf1d(self):
+    def test_sparse_varf1d(self):
         degree, degreef_max = 50, 7
         x = sym.symbols('x')
         function = 0
@@ -369,159 +366,9 @@ class TestSparseFunctions(unittest.TestCase):
                         print(i, j, v)
             self.assertEqual(bw, deg)
 
-    def testSpVarf2d(self):
+    def test_sparse_varf_2d(self):
         degree = 50
         function = 1. + self.v[0] + self.v[1] + self.v[1]*self.v[0]
         sp_var = self.quad2.varf(function, degree, sparse=True)
         var = self.quad2.varf(function, degree)
         self.assertAlmostEqual(la.norm(var - sp_var, 2), 0)
-
-
-class TestConvergenceFokkerPlanck1d(unittest.TestCase):
-
-    def setUp(self):
-
-        # Equation parameters
-        equation = eq.Fokker_Planck_1d
-        self.x, self.f = equation.x, equation.f
-
-        # Inverse temperature
-        self.β = sym.Rational(3)
-
-        # Common parameters of numerical approximation
-        self.degree = 100
-        self.n_points_num = 2*self.degree + 1
-
-    def sym_calc(self, Vp, m, s2):
-
-        # Equation parameters
-        equation = eq.Fokker_Planck_1d
-        x, f = equation.x, equation.f
-
-        # Calculation of the solution
-        new_q = hm.Quad.gauss_hermite
-
-        quad = new_q(self.n_points_num, dim=1, mean=[m], cov=[[s2]])
-
-        # Potential for approximation
-        Vq = sym.Rational(1/2)*(x-m)*(x-m)/(self.β*s2)
-
-        # Fokker Plarck operator
-        forward = equation.equation({'β': self.β, 'Vp': Vp})
-
-        # Map to appropriate space
-        factor = sym.exp(- self.β / 2 * (Vq + Vp))
-
-        # Mapped operator
-        backward = eq.map_operator(forward, f, factor)
-
-        # Discretize factor
-        factor = quad.discretize(factor)
-
-        return quad, forward, backward, factor
-
-    def solve(self, backward, quad, factor, degrees):
-
-        # Discretization of the operator
-        mat = quad.discretize_op(backward, self.f, self.degree, 2)
-
-        solutions = []
-        for d in degrees:
-            sub_mat = (mat[0:d+1, 0:d+1]).copy(order='C')
-            eig_vals, eig_vecs = las.eigs(sub_mat, k=1, which='LR')
-            ground_state = np.real(eig_vecs.T[0])
-            ground_state = ground_state * np.sign(ground_state[0])
-            ground_state_eval = quad.eval(quad.series(ground_state))*factor
-            norm = quad.norm(ground_state_eval, n=1, l2=True)
-            ground_state_eval = ground_state_eval / norm
-            solutions.append(ground_state_eval)
-        return solutions
-
-    def test_exact_sol_gaussian(self):
-
-        Vp, m, s2 = self.x*self.x/4, sym.Rational(1, 10), sym.Rational(1, 5)
-        quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
-        solution = eq.solve_gaussian(forward, self.f, [self.x])
-        norm_sol = quad.integrate(solution, l2=True)
-        self.assertTrue(abs(norm_sol - 1) < 1e-6)
-
-    def test_gaussian(self):
-
-        Vp, m, s2 = self.x*self.x/4, sym.Rational(1, 10), sym.Rational(1, 5)
-        quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
-
-        # Exact Solution
-        solution = eq.solve_gaussian(forward, self.f, [self.x])
-        solution_eval = quad.discretize(solution)
-
-        # Numerical solutions
-        degrees = list(range(5, self.degree))
-        solutions = self.solve(backward, quad, factor, degrees)
-
-        # Associated errors
-        errors = []
-        for sol in solutions:
-            error = quad.norm(sol - solution_eval, l2=True)
-            errors.append(error)
-
-        log_errors = np.log(errors)
-        poly_approx = np.polyfit(degrees, log_errors, 1)
-        errors_approx = np.exp(np.polyval(poly_approx, degrees))
-        error = la.norm(log_errors - np.log(errors_approx), 2)
-
-        plt.semilogy(degrees, errors, 'k.')
-        plt.semilogy(degrees, errors_approx)
-        # plt.show()
-
-        self.assertTrue(errors[-1] < 1e-12)
-        self.assertTrue(error < 5)
-
-    def test_norm_solution_bistable(self):
-
-        # Parameters of potential associated with Hermite polynomials
-        Vp = self.x**4/4 - self.x**2/2
-        m, s2 = sym.Rational(1/10), sym.Rational(1/10)
-        quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
-
-        sol_an = sym.exp(-self.β * Vp)
-        sol_num = quad.discretize(sol_an)
-        norm_1_an = quad.norm(sol_an, n=1, l2=True)
-        norm_1_num = quad.norm(sol_num, n=1, l2=True)
-        norm_2_an = quad.norm(sol_an, n=2, l2=True)
-        norm_2_num = quad.norm(sol_num, n=2, l2=True)
-        self.assertAlmostEqual(norm_1_an, norm_1_num)
-        self.assertAlmostEqual(norm_2_an, norm_2_num)
-
-    def test_bistable(self):
-
-        # Parameters of potential associated with Hermite polynomials
-        Vp = self.x**4/4 - self.x**2/2
-        m, s2 = sym.Rational(1/10), sym.Rational(1/10)
-        quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
-
-        # Exact Solution
-        exact_sol = sym.exp(-self.β * Vp)
-        norm = quad.norm(exact_sol, n=1, l2=True)
-        exact_sol = exact_sol / norm
-        solution_eval = quad.discretize(exact_sol)
-
-        degrees = list(range(10, self.degree))
-        solutions = self.solve(backward, quad, factor, degrees)
-
-        # Associated errors
-        errors = []
-        for sol in solutions:
-            error = quad.norm(sol - solution_eval, l2=True)
-            errors.append(error)
-
-        log_errors = np.log(errors)
-        poly_approx = np.polyfit(degrees, log_errors, 1)
-        errors_approx = np.exp(np.polyval(poly_approx, degrees))
-        error = la.norm(log_errors - np.log(errors_approx), 2)
-
-        plt.semilogy(degrees, errors, 'k.')
-        plt.semilogy(degrees, errors_approx)
-        # plt.show()
-
-        self.assertTrue(errors[-1] < 1e-10)
-        self.assertTrue(error < 10)
