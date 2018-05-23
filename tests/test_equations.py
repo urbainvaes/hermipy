83904bb8213aa6cad945341ec7abf39d1eb7715e
Urbain Vaes <urbain@vaes.uk>
2018-05-23 15:08:33 +0100

    Add test of exact solution for Fokker-Planck in 2d

--- tests/test_equations.py
+++ tests/test_equations.py
@@ -45,7 +45,7 @@ class TestConvergenceFokkerPlanck1d(unittest.TestCase):
         # Potential for approximation
         Vq = sym.Rational(1/2)*(x-m)*(x-m)/(self.β*s2)
 
-        # Fokker Plarck operator
+        # Fokker Planck operator
         forward = equation.equation({'β': self.β, 'Vp': Vp})
 
         # Map to appropriate space
@@ -166,73 +166,79 @@ class TestConvergenceFokkerPlanck1d(unittest.TestCase):
         self.assertTrue(error < 10)
 
 
-# class TestConvergenceFokkerPlanck2d(unittest.TestCase):
+class TestConvergenceFokkerPlanck2d(unittest.TestCase):
 
-#     def setUp(self):
+    def setUp(self):
 
-#         # Equation parameters
-#         equation = eq.McKean_Vlasov
-#         self.x, self.y, self.f = equation.x, equation.y, equation.f
+        # Equation parameters
+        equation = eq.McKean_Vlasov
+        self.x, self.y, self.f = equation.x, equation.y, equation.f
 
-#         # Inverse temperature
-#         self.β = sym.Rational(3)
+        # Common parameters of numerical approximation
+        self.degree = 50
+        self.n_points_num = 2*self.degree + 1
 
-#         # Common parameters of numerical approximation
-#         self.degree = 50
-#         self.n_points_num = 2*self.degree + 1
+    def sym_calc(self, Vp, parameters, m, s2):
 
-#     def sym_calc(self, Vp, m, s2):
+        # Equation parameters
+        β = parameters['β']
 
-#         # Equation parameters
-#         equation = eq.McKean_Vlasov
-#         x, y, f = equation.x, equation.y, equation.f
+        equation = eq.McKean_Vlasov
+        x, y, f = equation.x, equation.y, equation.f
 
-#         # Calculation of the solution
-#         new_q = hm.Quad.gauss_hermite
+        # Calculation of the solution
+        new_q = hm.Quad.gauss_hermite
+        quad = new_q(self.n_points_num, dim=2,
+                     mean=[m, 0], cov=[[s2, 0], [0, 1/sym.sqrt(2)]])
 
-#         quad = new_q(self.n_points_num, dim=1, mean=[m], cov=[[s2]])
+        # Potential for approximation
+        Vq = sym.Rational(1/2)*(x-m)*(x-m)/(β*s2)
 
-#         # Potential for approximation
-#         Vq = sym.Rational(1/2)*(x-m)*(x-m)/(self.β*s2)
+        # Fokker Planck for McKean-Vlasov equation
+        parameters.update({'Vp': Vp})
+        forward = equation.equation(parameters)
 
-#         # Fokker Plarck operator
-#         forward = equation.equation({'β': self.β, 'Vp': Vp})
+        # Map to appropriate space
+        factor_x = sym.exp(- β / 2 * (Vq + Vp))
+        factor_y = sym.exp(- sym.sqrt(2)*y*y)
+        factor = factor_x * factor_y
 
-#         # Map to appropriate space
-#         factor = sym.exp(- self.β / 2 * (Vq + Vp))
+        # Mapped operator
+        backward = eq.map_operator(forward, f, factor)
 
-#         # Mapped operator
-#         backward = eq.map_operator(forward, f, factor)
+        # Discretize factor
+        factor_x = quad.project(0).discretize(factor_x)
+        factor_y = quad.project(1).discretize(factor_y)
+        factor = quad.discretize(factor)
 
-#         # Discretize factor
-#         factor = quad.discretize(factor)
+        return quad, forward, backward, factor, factor_x, factor_y
 
-#         return quad, forward, backward, factor
+    def solve(self, backward, quad, factor, degrees):
 
-#     def solve(self, backward, quad, factor, degrees):
+        # Discretization of the operator
+        mat = quad.discretize_op(backward, self.f, self.degree, 2)
 
-#         # Discretization of the operator
-#         mat = quad.discretize_op(backward, self.f, self.degree, 2)
+        solutions = []
+        for d in degrees:
+            sub_mat = (mat[0:d+1, 0:d+1]).copy(order='C')
+            eig_vals, eig_vecs = las.eigs(sub_mat, k=1, which='LR')
+            ground_state = np.real(eig_vecs.T[0])
+            ground_state = ground_state * np.sign(ground_state[0])
+            ground_state_eval = quad.eval(quad.series(ground_state))*factor
+            norm = quad.norm(ground_state_eval, n=1, l2=True)
+            ground_state_eval = ground_state_eval / norm
+            solutions.append(ground_state_eval)
+        return solutions
 
-#         solutions = []
-#         for d in degrees:
-#             sub_mat = (mat[0:d+1, 0:d+1]).copy(order='C')
-#             eig_vals, eig_vecs = las.eigs(sub_mat, k=1, which='LR')
-#             ground_state = np.real(eig_vecs.T[0])
-#             ground_state = ground_state * np.sign(ground_state[0])
-#             ground_state_eval = quad.eval(quad.series(ground_state))*factor
-#             norm = quad.norm(ground_state_eval, n=1, l2=True)
-#             ground_state_eval = ground_state_eval / norm
-#             solutions.append(ground_state_eval)
-#         return solutions
+    def test_exact_sol_gaussian(self):
 
-#     def test_exact_sol_gaussian(self):
-
-#         Vp, m, s2 = self.x*self.x/4, sym.Rational(1, 10), sym.Rational(1, 5)
-#         quad, forward, backward, factor = self.sym_calc(Vp, m, s2)
-#         solution = eq.solve_gaussian(forward, self.f, [self.x])
-#         norm_sol = quad.integrate(solution, l2=True)
-#         self.assertTrue(abs(norm_sol - 1) < 1e-6)
+        r = sym.Rational
+        params = {'β': r(3), 'ε': r(1), 'γ': 0, 'θ': 0, 'm': 0}
+        Vp, m, s2 = self.x*self.x/4, sym.Rational(1, 10), sym.Rational(1, 5)
+        quad, forward, _, factor, _, _ = self.sym_calc(Vp, params, m, s2)
+        solution = eq.solve_gaussian(forward, self.f, [self.x, self.y])
+        norm_sol = quad.norm(solution, n=1, l2=True)
+        self.assertTrue(abs(norm_sol - 1) < 1e-6)
 
 #     def test_gaussian(self):
 
