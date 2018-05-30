61ae83c49aadf30d35324dc431dfbe6daff03afb
Urbain Vaes <urbain@vaes.uk>
2018-05-30 11:23:38 +0100

    Fix minor bug in test_equations

--- tests/test_equations.py
+++ tests/test_equations.py
@@ -181,8 +181,14 @@ class TestConvergenceFokkerPlanck2d(unittest.TestCase):
         equation = eq.McKean_Vlasov
         self.x, self.y, self.f = equation.x, equation.y, equation.f
 
+        # Default degree
+        self.degree = 50
+
     def sym_calc(self, Vp, parameters, m, s2x, s2y,
-                 degree=50):
+                 degree=None):
+
+        # Defalut degree
+        degree = degree or self.degree
 
         # Number of quadrature points
         n_points_num = 2*degree + 1
@@ -230,7 +236,7 @@ class TestConvergenceFokkerPlanck2d(unittest.TestCase):
 
         v0, eig_vec = None, None
         for d in degrees:
-            print(d)
+            # print(d)
             npolys = int(binom(d + 2, d))
             if d is not degrees[0]:
                 v0 = np.zeros(npolys)
@@ -295,12 +301,130 @@ class TestConvergenceFokkerPlanck2d(unittest.TestCase):
         errors_approx = np.exp(np.polyval(poly_approx, degrees))
         error = la.norm(log_errors - np.log(errors_approx), 2)
 
-        plt.semilogy(degrees[0:-1], errors, 'k.')
-        plt.semilogy(degrees[0:-1], errors_approx)
+        plt.semilogy(degrees, errors, 'k.')
+        plt.semilogy(degrees, errors_approx)
         # plt.show()
 
         self.assertTrue(errors[-1] < 1e-6)
         self.assertTrue(error < 1)
+
+    def test_bistable(self):
+
+        r = sym.Rational
+        Vp, degree = self.x**4/4 - self.x**2/2, 40
+        m, s2x, s2y = r(1, 10), r(1, 20), 1/2
+        params = {'β': r(10), 'ε': r(.5), 'γ': 0, 'θ': 0, 'm': 0}
+        args = [Vp, params, m, s2x, s2y, degree]
+        quad, forward, backward, factor, _, _ = self.sym_calc(*args)
+
+        # Numerical solutions
+        degrees = list(range(5, degree))
+        solutions, finest = self.solve(backward, quad, factor, degrees)
+        finest_eval = solutions[-1]
+
+        # Plot of the finest solution
+        # fig, ax = plt.subplots(1, 1)
+        # quad.plot(finest, factor, ax=ax)
+        # plt.show()
+
+        # Associated errors
+        errors, degrees = [], degrees[0:-1]
+        for sol in solutions[0:-1]:
+            error = quad.norm(sol - finest_eval, l2=True)
+            errors.append(error)
+            # print(error)
+
+        log_errors = np.log(errors)
+        poly_approx = np.polyfit(degrees, log_errors, 1)
+        errors_approx = np.exp(np.polyval(poly_approx, degrees))
+        error = la.norm(log_errors - np.log(errors_approx), 2)
+
+        # plt.semilogy(degrees, errors, 'k.')
+        # plt.semilogy(degrees, errors_approx)
+        # plt.show()
+
+        self.assertTrue(errors[-1] < 1e-3)
+        self.assertTrue(error < 3)
+
+
+class TestConvergenceFokkerPlanck3d(unittest.TestCase):
+
+    def setUp(self):
+
+        # Equation parameters
+        equation = eq.McKean_Vlasov_harmonic_noise
+        self.x, self.y, self.z = equation.x, equation.y, equation.z
+        self.f = equation.f
+
+    def sym_calc(self, Vp, parameters, s2x, s2y, s2z, degree=10):
+
+        # Number of quadrature points
+        n_points_num = 2*degree + 1
+
+        # Equation parameters
+        β = parameters['β']
+
+        equation = eq.McKean_Vlasov
+        x, y, z, f = self.x, self.y, self.z, self.f
+
+        # Calculation of the solution
+        new_q = hm.Quad.gauss_hermite
+        cov = [[s2x, 0, 0], [0, s2y, 0], [0, 0, s2z]]
+        quad = new_q(n_points_num, dim=2, mean=[0]*3, cov=cov)
+
+        # Potential for approximation
+        Vqx = sym.Rational(1/2)*x*x/(β*s2x)
+        Vqy = sym.Rational(1/2)*y*y/s2y
+        Vqz = sym.Rational(1/2)*z*z/s2z
+
+        # Fokker Planck for McKean-Vlasov equation
+        parameters.update({'Vp': Vp})
+        forward = equation.equation(parameters)
+
+        # Map to appropriate space
+        factor_x = sym.exp(- β / 2 * (Vqx + Vp))
+        factor_yz = sym.exp(- 1/2 * (y*y/2 + Vqy + z*z/2 + Vqz))
+        factor = factor_x * factor_yz
+
+        # Mapped operator
+        backward = eq.map_operator(forward, f, factor)
+
+        # Discretize factor
+        factor_x = quad.project(0).discretize(factor_x)
+        factor = quad.discretize(factor)
+
+        return quad, forward, backward, factor, factor_x
+
+    def solve(self, backward, quad, factor, degrees):
+
+        # Discretization of the operator
+        mat = quad.discretize_op(backward, self.f, degrees[-1], 2)
+
+        solutions = []
+
+        v0, eig_vec = None, None
+        for d in degrees:
+            print(d)
+            npolys = int(binom(d + 2, d))
+            if d is not degrees[0]:
+                v0 = np.zeros(npolys)
+                for i in range(len(eig_vec)):
+                    v0[i] = eig_vec[i]
+            sub_mat = (mat[0:npolys, 0:npolys]).copy(order='C')
+            # pdb.set_trace()
+            eig_vals, eig_vecs = las.eigs(sub_mat, k=1, v0=v0, which='LR')
+            eig_vec = np.real(eig_vecs.T[0])
+            ground_state = eig_vec * np.sign(eig_vec[0])
+            ground_state_eval = quad.eval(quad.series(ground_state))*factor
+            norm = quad.norm(ground_state_eval, n=1, l2=True)
+            ground_state_eval = ground_state_eval / norm
+            solutions.append(ground_state_eval)
+
+            # fig, ax = plt.subplots(1, 1)
+            # quad.plot(quad.series(ground_state), factor, ax=ax)
+            # plt.show()
+
+        return solutions, quad.series(ground_state)
 
     def test_bistable(self):
 
