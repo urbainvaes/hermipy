48fdefa5e9801f0b255de23125d5db0e6b6387b8
Urbain Vaes <urbain@vaes.uk>
2018-06-03 19:30:01 +0100

    Move composite quad to separate folder

--- hermite/quad.py
+++ hermite/quad.py
@@ -267,22 +267,3 @@ class Quad:
                          cov=self.cov,
                          degree=degree,
                          norm=norm)
-# }}}
-#  Composite quadrature {{{
-
-
-class CompQuad:
-    def __init__(self, quads, weights):
-        self.quads = quads
-        self.weights = weights
-
-    # def integrate(f):
-
-    # def eval(self, degree, nodes):
-    #     return eval_simple_quad(self.coeffs, degree, nodes)
-
-# def herm_to_poly(c):
-#     herme_coeffs = c/np.sqrt(np.sqrt(2*np.pi)*np.arange(len(c)))
-#     return herm.herme2poly(herme_coeffs)
-# }}}
-# vim: foldmethod=marker foldnestmax=2
