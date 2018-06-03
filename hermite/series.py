9f5971e84a9305fab3d8e1c3cd7ee09b6bb67691
Urbain Vaes <urbain@vaes.uk>
2018-06-03 19:33:54 +0100

    Add functions for addition and tensorization in Series

--- hermite/series.py
+++ hermite/series.py
@@ -1,25 +1,15 @@
 import hermite.core as core
+import hermite.lib as lib
+
 from scipy.special import binom
 import numpy as np
 import numpy.linalg as la
 
 
-class Series:
+very_small = 1e-10
 
-    @staticmethod
-    def natural_bissect(func, x1=0, x2=1000):
-        f1, f2 = func(x1), func(x2)
-        if f1 is 0:
-            return x1
-        elif f2 is 0:
-            return x2
-        assert f1*f2 < 0
-        x3 = (x1+x2)//2
-        f3 = func(x3)
-        replace_arg = 'x2' if f1*f3 <= 0 else 'x1'
-        new_args = {'x1': x1, 'x2': x2}
-        new_args[replace_arg] = x3
-        return Series.natural_bissect(func, **new_args)
+
+class Series:
 
     def __init__(self, coeffs, dim=1, mean=None, cov=None,
                  degree=None, norm=False):
@@ -34,18 +24,62 @@ class Series:
         eigval, eigvec = la.eig(self.cov)
         self.factor = np.matmul(eigvec, np.sqrt(np.diag(eigval)))
 
+        diag_cov = np.diag(np.diag(self.cov))
+        self.is_diag = la.norm(self.cov - diag_cov, 2) < 1e-10
+
         if degree is None:
-            self.degree = Series.natural_bissect(
+            self.degree = lib.natural_bissect(
                     lambda x: int(binom(x + self.dim, x)) - len(self.coeffs))
         else:
             self.degree = degree
 
+    def __eq__(self, other):
+        assert type(other) is Series
+        return self.dim == other.dim \
+            and la.norm(self.mean - other.mean, 2) < very_small \
+            and la.norm(self.cov - other.cov, 2) < very_small \
+            and la.norm(self.coeffs - other.coeffs) < very_small
+
     def __add__(self, other):
-        assert abs(self.dim - other.dim) < 1e-8
-        assert la.norm(self.mean - other.mean, 2) < 1e-8
-        assert la.norm(self.cov - other.cov, 2) < 1e-8
-        new_coeffs = self.coeffs + other.coeffs
+
+        if isinstance(other, (float, np.float64)):
+            new_coeffs = self.coeffs + other
+
+        elif type(other) is Series:
+            assert self.dim == other.dim
+            assert la.norm(self.mean - other.mean, 2) < very_small
+            assert la.norm(self.cov - other.cov, 2) < very_small
+            new_coeffs = self.coeffs + other.coeffs
+
+        else:
+            raise TypeError("Invalid type!)")
+
         return Series(new_coeffs, dim=self.dim, mean=self.mean, cov=self.cov)
+
+    def __mul__(self, other):
+
+        if isinstance(other, (float, np.float64)):
+            new_coeffs = self.coeffs * other
+            return Series(new_coeffs, dim=self.dim,
+                          mean=self.mean, cov=self.cov)
+
+        elif type(other) is Series:
+            assert self.is_diag and other.is_diag
+            dim = self.dim + other.dim
+            mean = np.zeros(dim)
+            cov = np.zeros((dim, dim))
+            for i in range(self.dim):
+                mean[i] = self.mean[i]
+                cov[i][i] = self.cov[i][i]
+            for i in range(other.dim):
+                off = self.dim
+                mean[off + i] = other.mean[i]
+                cov[off + i][off + i] = other.cov[i][i]
+            coeffs = core.tensorize([self.coeffs, other.coeffs])
+            return Series(coeffs, dim=dim, mean=mean, cov=cov)
+
+        else:
+            raise TypeError("Invalid type: " + str(type(other)))
 
     def project(self, direction):
         direction = core.to_numeric(direction)
