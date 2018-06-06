01305a57f06c35bb73f8970c4163e6a622a94b27
Urbain Vaes <urbain@vaes.uk>
2018-06-06 16:08:36 +0100

    Add inner product for Hermite series

--- hermite/series.py
+++ hermite/series.py
@@ -1,3 +1,5 @@
+#  TODO: Add directions in Position (urbain, 06 Jun 2018)
+
 import hermite.core as core
 import hermite.lib as lib
 import hermite.position as pos
@@ -23,6 +25,15 @@ class Series:
         tens_vec = core.tensorize(vecs)
         tens_pos = pos.Position.tensorize([a.position for a in args])
         return Series(tens_vec, tens_pos)
+
+    @staticmethod
+    def inner(s1, s2):
+        assert type(s1) is Series and type(s2) is Series
+        assert s1.degree == s2.degree
+        d1, d2 = s1.position.dirs, s2.position.dirs
+        result = core.inner(s1.coeffs, s2.coeffs, d1, d2)
+        inner_pos = pos.Position.inner(s1.position, s2.position)
+        return Series(result, inner_pos, degree=s1)
 
     def __init__(self, coeffs, position, degree=None, norm=False):
         self.coeffs = coeffs/la.norm(coeffs, 2) if norm else coeffs
