6c537677dde06c50582a55ed8ad3dd488e702850
Urbain Vaes <urbain@vaes.uk>
2018-06-13 23:36:12 +0100

    Implement hyperbolic cross iterator

--- cpp/src/hermite/iterators.cpp
+++ cpp/src/hermite/iterators.cpp
@@ -18,6 +18,11 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
+// FIXME: Last multi-index twice? (urbain, 13 Jun 2018)
+
+
+#include <unordered_map>
+#include "hermite/io.hpp"
 #include "hermite/types.hpp"
 #include "hermite/iterators.hpp"
 
@@ -26,55 +31,102 @@
 namespace hermite
 {
 
-Hyperbolic_cross_iterator::Hyperbolic_cross_iterator(u_int dim, u_int upper_bound)
-    : Vector_iterator(dim), upper_bound(upper_bound) 
+u_int hash_multi_ind(ivec v, int degree)
 {
+    u_int base = degree + 1;
+    u_int result = 0;
+    u_int unit = 1;
+    for(u_int i = 0; i < v.size(); i++)
+    {
+        result += v[i]*unit;
+        unit *= base;
+    }
+    return result;
+}
 
+#define MAX(i, j) (i > j ? i : j)
+bool Hyperbolic_cross_increment(ivec & multi_index, u_int dim, u_int upper_bound)
+{
+    u_int i;
+
+    for (i = 0; i < dim; i++)
+    {
+        if (multi_index[i] == 1)
+            multi_index[i] = 0;
+
+        else if (multi_index[i] == 0)
+        {
+            multi_index[i] = 1;
+            return false;
+        }
+    }
+
+    for (i = 0; i < dim - 1; i++)
+    {
+        if (multi_index[i + 1] == 0)
+            continue;
+
+        bool found_factor = false;
+        u_int factor;
+        for (u_int f : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31})
+        {
+            if (multi_index[i + 1] % f == 0)
+            {
+                found_factor = true;
+                factor = f;
+                break;
+            }
+        }
+        if (!found_factor)
+            factor = multi_index[i+1];
+
+        bool is_prime = factor == multi_index[i+1];
+
+        multi_index[i] = MAX(1, multi_index[0]) * factor;
+        multi_index[i + 1] /= factor;
+
+        if (is_prime)
+            multi_index[i + 1] = 0;
+
+        break;
+    }
+
+    if (i == dim - 1)
+    {
+        if (multi_index[0] == upper_bound)
+            return true;
+        multi_index[i] = 1 + MAX(1, multi_index[0]);
+    }
+
+    if (i > 0)
+        multi_index[0] = 0;
+
+    return false;
+}
+
+Hyperbolic_cross_iterator::Hyperbolic_cross_iterator(u_int dim, u_int upper_bound)
+    : Vector_iterator(dim), upper_bound(upper_bound), index_list(0)
+{
+    ivec m(dim, 0);
+    u_int ind = 0;
+    do
+    {
+        list.push_back(m);
+        u_int hash = hash_multi_ind(m, upper_bound);
+        hash_table.insert(std::pair<u_int, u_int>(hash, ind++));
+    }
+    while (!Hyperbolic_cross_increment(m, dim, upper_bound));
 }
 
 void Hyperbolic_cross_iterator::increment()
 {
-    u_int i, prod;
-    for (i = 0; i < dim; i++)
-        prod *= multi_index[i] == 0 ? 1 : multi_index[i];
-
-    for (i = 0; i < dim - 1; i++)
-    {
-        if (multi_index[i + 1]  != 0)
-        {
-            multi_index[i + 1] -= 1;
-            break;
-        }
-    }
-
-    if (i == dim - 1 && multi_index[0] == upper_bound)
+    if (index_list == list.size())
     {
         full = true;
         return;
     }
 
-    multi_index[i] = 1 + multi_index[0];
-
-    if (i > 0)
-    {
-        multi_index[0] = 0;
-    }
-}
-
-
-u_int Multi_index_iterator::index(const ivec & m_vec)
-{
-    using boost::math::binomial_coefficient;
-    u_int sum = 0, result = 0;
-    for (u_int i = 0; i < m_vec.size(); i++)
-    {
-        sum += m_vec[i];
-        if (sum > 0)
-        {
-            result += (u_int) binomial_coefficient<double> (i + sum, i + 1);
-        }
-    }
-    return result;
+    multi_index = list[++index_list];
 }
 
 void Multi_index_iterator::increment()
@@ -145,6 +197,41 @@ u_int Multi_index_iterator::size(u_int degree, u_int dim)
 {
     using boost::math::binomial_coefficient;
     return (u_int) binomial_coefficient<double> (degree + dim, dim);
+}
+
+u_int Multi_index_iterator::index(const ivec & m_vec)
+{
+    using boost::math::binomial_coefficient;
+    u_int sum = 0, result = 0;
+    for (u_int i = 0; i < m_vec.size(); i++)
+    {
+        sum += m_vec[i];
+        if (sum > 0)
+        {
+            result += (u_int) binomial_coefficient<double> (i + sum, i + 1);
+        }
+    }
+    return result;
+}
+
+u_int Hyperbolic_cross_iterator::index(const ivec & m_vec)
+{
+    u_int hash_mvec = hash_multi_ind(m_vec, upper_bound);
+    return hash_table.at(hash_mvec);
+}
+
+u_int Hyper_cube_iterator::index(const ivec & m_vec)
+{
+    u_int result = 0,
+          factor = 1;
+
+    for (u_int i = 0; i < dim; i++)
+    {
+        u_int position = dim - 1 - i;
+        result += factor * m_vec[position];
+        factor *= upper_bounds[position];
+    }
+    return result;
 }
 
 }
