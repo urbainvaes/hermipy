1a0bbbeff009fa1b7417ca7257c5109cca41fe12
Urbain Vaes <urbain@vaes.uk>
2018-06-15 00:53:02 +0100

    Make inner work with different iterators (not tested)

--- cpp/src/hermite/iterators.cpp
+++ cpp/src/hermite/iterators.cpp
@@ -27,6 +27,52 @@
 
 namespace hermite
 {
+// Grid iterators (used for N-dim for loops)
+// Hypercube iterator {{{
+u_int Hyper_cube_iterator::index(const ivec & m_vec)
+{
+    u_int result = 0,
+          factor = 1;
+
+    for (u_int i = 0; i < dim; i++)
+    {
+        u_int position = dim - 1 - i;
+        result += factor * m_vec[position];
+        factor *= upper_bounds[position];
+    }
+    return result;
+}
+
+void Hyper_cube_iterator::increment()
+{
+    unsigned int i = dim - 1;
+    while(multi_index[i] == upper_bounds[i] - 1 && i > 0)
+    {
+        multi_index[i] = 0;
+        i -= 1;
+    }
+    if(i == 0 && multi_index[0] == upper_bounds[0] - 1)
+    {
+        full = true;
+    }
+    else
+    {
+        multi_index[i] += 1;
+    }
+}
+
+imat Hyper_cube_iterator::list(const ivec & upper_bounds)
+{
+    imat result;
+    for(Hyper_cube_iterator m(upper_bounds); !m.isFull(); m.increment())
+    {
+        result.push_back(m.get());
+    }
+    return result;
+}
+
+// }}}
+
 // Multi-index iterators
 // Parent class {{{
 u_int Multi_index_iterator::index(const ivec & m_vec)
@@ -50,6 +96,77 @@ void Multi_index_iterator::increment()
     }
 
     multi_index = list[++index_list];
+}
+// }}}
+// Triangle iterator {{{
+bool Triangle_iterator::s_increment(ivec & multi_index, u_int dim, u_int upper_bound)
+{
+    u_int i;
+    for (i = 0; i < dim - 1; i++)
+    {
+        if (multi_index[i + 1]  != 0)
+        {
+            multi_index[i + 1] -= 1;
+            break;
+        }
+    }
+
+    if (i == dim - 1 && multi_index[0] == upper_bound)
+        return true;
+
+    multi_index[i] = 1 + multi_index[0];
+
+    if (i > 0)
+    {
+        multi_index[0] = 0;
+    }
+
+    return false;
+}
+
+Triangle_iterator::Triangle_iterator(u_int dim, u_int upper_bound)
+    : Multi_index_iterator(dim), upper_bound(upper_bound)
+{
+    ivec m(dim, 0);
+    u_int ind = 0;
+    do
+    {
+        list.push_back(m);
+        std::string hash = hash_print(m);
+        hash_table.insert(std::pair<std::string, u_int>(hash, ind++));
+    }
+    while (!Triangle_iterator::s_increment(m, dim, upper_bound));
+}
+
+u_int Triangle_iterator::s_index(const ivec & m_vec)
+{
+    using boost::math::binomial_coefficient;
+    u_int sum = 0, result = 0;
+    for (u_int i = 0; i < m_vec.size(); i++)
+    {
+        sum += m_vec[i];
+        if (sum > 0)
+        {
+            result += (u_int) binomial_coefficient<double> (i + sum, i + 1);
+        }
+    }
+    return result;
+}
+
+u_int Triangle_iterator::s_size(u_int degree, u_int dim)
+{
+    using boost::math::binomial_coefficient;
+    return (u_int) binomial_coefficient<double> (degree + dim, dim);
+}
+
+imat Triangle_iterator::s_list(u_int dim, u_int upper_bound)
+{
+    imat result;
+    for(Triangle_iterator m(dim, upper_bound); !m.isFull(); m.increment())
+    {
+        result.push_back(m.get());
+    }
+    return result;
 }
 // }}}
 // Hyperbolic cross iterator {{{
@@ -131,122 +248,6 @@ imat Cross_iterator::s_list(u_int dim, u_int upper_bound)
 {
     imat result;
     for(Cross_iterator m(dim, upper_bound); !m.isFull(); m.increment())
-    {
-        result.push_back(m.get());
-    }
-    return result;
-}
-// }}}
-// Triangle iterator {{{
-bool Triangle_iterator::s_increment(ivec & multi_index, u_int dim, u_int upper_bound)
-{
-    u_int i;
-    for (i = 0; i < dim - 1; i++)
-    {
-        if (multi_index[i + 1]  != 0)
-        {
-            multi_index[i + 1] -= 1;
-            break;
-        }
-    }
-
-    if (i == dim - 1 && multi_index[0] == upper_bound)
-        return true;
-
-    multi_index[i] = 1 + multi_index[0];
-
-    if (i > 0)
-    {
-        multi_index[0] = 0;
-    }
-
-    return false;
-}
-
-Triangle_iterator::Triangle_iterator(u_int dim, u_int upper_bound)
-    : Multi_index_iterator(dim), upper_bound(upper_bound)
-{
-    ivec m(dim, 0);
-    u_int ind = 0;
-    do
-    {
-        list.push_back(m);
-        std::string hash = hash_print(m);
-        hash_table.insert(std::pair<std::string, u_int>(hash, ind++));
-    }
-    while (!Triangle_iterator::s_increment(m, dim, upper_bound));
-}
-
-u_int Triangle_iterator::s_index(const ivec & m_vec)
-{
-    using boost::math::binomial_coefficient;
-    u_int sum = 0, result = 0;
-    for (u_int i = 0; i < m_vec.size(); i++)
-    {
-        sum += m_vec[i];
-        if (sum > 0)
-        {
-            result += (u_int) binomial_coefficient<double> (i + sum, i + 1);
-        }
-    }
-    return result;
-}
-
-u_int Triangle_iterator::s_size(u_int degree, u_int dim)
-{
-    using boost::math::binomial_coefficient;
-    return (u_int) binomial_coefficient<double> (degree + dim, dim);
-}
-
-imat Triangle_iterator::s_list(u_int dim, u_int upper_bound)
-{
-    imat result;
-    for(Triangle_iterator m(dim, upper_bound); !m.isFull(); m.increment())
-    {
-        result.push_back(m.get());
-    }
-    return result;
-}
-// }}}
-
-// Grid iterators (used for N-dim for loops)
-// Hypercube iterator {{{
-u_int Hyper_cube_iterator::index(const ivec & m_vec)
-{
-    u_int result = 0,
-          factor = 1;
-
-    for (u_int i = 0; i < dim; i++)
-    {
-        u_int position = dim - 1 - i;
-        result += factor * m_vec[position];
-        factor *= upper_bounds[position];
-    }
-    return result;
-}
-
-void Hyper_cube_iterator::increment()
-{
-    unsigned int i = dim - 1;
-    while(multi_index[i] == upper_bounds[i] - 1 && i > 0)
-    {
-        multi_index[i] = 0;
-        i -= 1;
-    }
-    if(i == 0 && multi_index[0] == upper_bounds[0] - 1)
-    {
-        full = true;
-    }
-    else
-    {
-        multi_index[i] += 1;
-    }
-}
-
-imat Hyper_cube_iterator::list(const ivec & upper_bounds)
-{
-    imat result;
-    for(Hyper_cube_iterator m(upper_bounds); !m.isFull(); m.increment())
     {
         result.push_back(m.get());
     }
