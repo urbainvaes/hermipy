2cf326d541c8b43084379ddc5aa92238bcf2aad3
Urbain Vaes <urbain@vaes.uk>
2018-07-12 18:53:06 +0100

    Update for push

--- examples/config.py
+++ examples/config.py
@@ -29,10 +29,10 @@ x, y, f = equation.x, equation.y, equation.f
 r = sym.Rational
 
 # Configuration of numerical method
-num['degree'] = 30  # degree of approximation
+num['degree'] = 40  # degree of approximation
 num['n_points_num'] = 2*num['degree'] + 1  # (*2 for varf)
 num['μx'] = r(1, 5)
-num['σx'] = r(1, 2)
+num['σx'] = r(1, 10)
 num['λ'] = r(1, 2)
 
 # Scalar parameters of the equation
@@ -42,11 +42,11 @@ eq['γ'] = r(0)
 eq['θ'] = r(0)
 
 # Functional parameters of the equation
-# eq['Vp'] = x**4/4 - x**2/2
+eq['Vp'] = x**4/4 - x**2/2
 
-sx = sym.symbols('sx', real=True, positive=True)
-eq['sx'] = r(1)
-eq['Vp'] = r(.5)*x*x/sx
+# sx = sym.symbols('sx', real=True, positive=True)
+# eq['sx'] = r(1)
+# eq['Vp'] = r(.5)*x*x/sx
 
 # Miscellaneous parameters
 misc['cache'] = False
