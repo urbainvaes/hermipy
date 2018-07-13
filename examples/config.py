d6bfee13ccf995c18eddd24a42a70d1e123c35a5
Urbain Vaes <urbain@vaes.uk>
2018-07-13 21:26:42 +0100

    Working version of convergence for OU noise

--- examples/config.py
+++ examples/config.py
@@ -29,7 +29,7 @@ x, y, f = equation.x, equation.y, equation.f
 r = sym.Rational
 
 # Configuration of numerical method
-num['degree'] = 40  # degree of approximation
+num['degree'] = 100  # degree of approximation
 num['n_points_num'] = 2*num['degree'] + 1  # (*2 for varf)
 num['μx'] = r(1, 5)
 num['σx'] = r(1, 10)
@@ -42,16 +42,17 @@ eq['γ'] = r(0)
 eq['θ'] = r(0)
 
 # Functional parameters of the equation
-eq['Vp'] = x**4/4 - x**2/2
+# eq['Vp'] = x**4/4 - x**2/2
 
-# sx = sym.symbols('sx', real=True, positive=True)
-# eq['sx'] = r(1)
-# eq['Vp'] = r(.5)*x*x/sx
+sx = sym.symbols('sx', real=True, positive=True)
+eq['sx'] = r(1)
+eq['Vp'] = r(.5)*x*x/sx
 
 # Miscellaneous parameters
 misc['cache'] = False
+misc['debug'] = False
 misc['parallel'] = False
-misc['tensorize'] = False
-misc['trails'] = False
+misc['tensorize'] = True
+misc['trails'] = True
 misc['plots'] = True
 misc['symbolic'] = 0  # Values 0, 1, 2
