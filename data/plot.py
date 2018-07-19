84c667fbe4abe1a80784cb826529f5f469d1fc8c
Urbain Vaes <urbain@vaes.uk>
2018-07-19 16:11:32 +0100

    Add data for paper

---
+++ data/plot.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.rc('font', size=14)
+matplotlib.rc('font', family='serif')
+matplotlib.rc('text', usetex=True)
+
+epsilon, v, errors = [], [], []
+for i in range(0, 6):
+    epsilon.append(1/2**i)
+    v.append(np.load("errors-epsilon-{}.npy".format(epsilon[-1])))
+    errors.append(v[-1][-1])
+epsilon, errors = np.array(epsilon), np.array(errors)
+
+for i, y in enumerate(v):
+    plt.semilogy(range(5, 5 + len(y)), y, '.', basey=2, 
+                 label='$\\varepsilon = {}$'.format(epsilon[i]))
+plt.legend()
+plt.legend(loc='upper right')
+plt.xlabel("Degree of approximation")
+plt.savefig('degree-difference.eps', bbox_inches='tight')
+plt.show()
+
+coeffs = np.polyfit(np.log2(epsilon), np.log2(errors), 1)
+plt.loglog(epsilon, 2**coeffs[1] * epsilon**coeffs[0], 'r-',
+            label='$y = {:.2f} \\times 2^{{ {:.2f} \\, x}}$'.
+                  format(2**coeffs[1], coeffs[0]), basey=2)
+plt.loglog(epsilon, errors, '.', basey=2, basex=2, markersize=10)
+plt.legend()
+plt.xlabel("$\\varepsilon$")
+plt.savefig('epsilon-error.eps', bbox_inches='tight')
+plt.show()
