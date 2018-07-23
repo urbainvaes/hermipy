9fbb44f84c19d432c45abbb3200414ada2cfb30f
Urbain Vaes <urbain@vaes.uk>
2018-07-23 13:22:08 +0100

    Improve Operator class

--- hermipy/operator.py
+++ hermipy/operator.py
@@ -22,24 +22,31 @@ import itertools
 import math
 
 
+# Linear operator!
 class Operator():
 
     f = sympy.Function('f')
 
-    def __init__(self, expr):
+    def __init__(self, expr, dirs=None):
 
         if isinstance(expr, Operator):
             self.sym = expr.sym
             self.dirs = expr.dirs
             return
 
-        function = func.Function(expr)
-        self.sym = function.sym
-        variables = self.sym.free_symbols.intersection(func.Function.x_sub)
-        self.dirs = sorted([func.Function.x_sub.index(s) for s in variables])
+        aux = func.Function(expr)
 
-        if not self.sym.has(self.f):
-            raise TypeError("Argument is not an operator on f" + str(self.sym))
+        if aux.sym == 0:
+            assert dirs is not None
+            self.sym, self.dirs = aux.sym, dirs
+            return
+
+        functions = list(aux.sym.atoms(sympy.function.AppliedUndef))
+        if len(functions) is not 1:
+            raise TypeError("There should be exactly 1 functional argument!")
+
+        self.sym, self.dirs = aux.sym.subs(functions[0].func, self.f), aux.dirs
+        assert len(self.dirs) is len(functions[0].args)
 
     def __eq__(self, other):
         return self.dirs == other.dirs and \
@@ -53,49 +60,53 @@ class Operator():
         if type(other) is not func.Function:
             other = func.Function(other, dirs=self.dirs)
         assert self.dirs == other.dirs
-        variables = [func.Function.x_sub[d] for d in self.dirs]
-        sym = self.sym.subs(self.f(*variables), other.sym)
-        return Operator(sym)
-
-    def __call__(self, arg):
-
-        if not isinstance(arg, (func.Function, Operator)):
-            try:
-                arg = Operator(arg)
-            except TypeError:
-                arg = func.Function(arg)
-
-        variables = [func.Function.x_sub[d] for d in self.dirs]
-        sym = self.sym.subs(self.f(*variables), arg.sym)
-        return Operator(sym) if type(arg) is Operator \
-            else func.Function(sym, dirs=self.dirs)
+        return Operator(self.sym*other.sym, dirs=self.dirs)
 
     def __add__(self, other):
         if type(other) is not Operator:
             other = Operator(other, dirs=self.dirs)
         assert self.dirs == other.dirs
-        sym = self.sym + other.sym
-        return Operator(sym, dirs=self.dirs)
+        return Operator(self.sym + other.sym, dirs=self.dirs)
+
+    def __sub__(self, other):
+        if type(other) is not Operator:
+            other = Operator(other, dirs=self.dirs)
+        assert self.dirs == other.dirs
+        return Operator(self.sym - other.sym, dirs=self.dirs)
+
+    def __neg__(self):
+        return Operator(- self.sym, dirs=self.dirs)
 
     __str__ = __repr__
     __radd__ = __add__
     __rmul__ = __mul__
 
+    def __call__(self, arg):
+
+        if not isinstance(arg, (func.Function, Operator)):
+            try:
+                arg = Operator(arg, dirs=self.dirs)
+            except TypeError:
+                arg = func.Function(arg, dirs=self.dirs)
+        variables = [func.Function.x_sub[d] for d in self.dirs]
+        sym = self.sym.subs(self.f(*variables), arg.sym).doit()
+        return Operator(sym, dirs=self.dirs) if type(arg) is Operator \
+            else func.Function(sym, dirs=self.dirs)
+
     def as_xyz(self):
-        return self.sym.subs(((func.Function.x_sub[i], func.Function.xyz[i])
-                              for i in range(len(func.Function.x_sub))))
+        sub, xyz = func.Function.x_sub, func.Function.xyz
+        return self.sym.subs(((sub[i], xyz[i]) for i in range(len(sub))))
 
     def split(self):
         MAX_ORDER = 5
         variables = [func.Function.x_sub[d] for d in self.dirs]
         unknown, dim = self.f(*variables), len(variables)
-        result, rem = [], self.sym.expand()
+        result, rem = {}, self.sym.expand()
         mult = list(m for m in itertools.product(range(MAX_ORDER + 1),
                     repeat=dim) if sum(m) <= MAX_ORDER)
         for m in mult:
             if rem == 0:
-                result.append(0)
-                continue
+                break
             test, der = 1, unknown
             for i, v in zip(m, variables):
                 test *= v**i/math.factorial(i)
@@ -110,16 +121,7 @@ class Operator():
                     term += termarg
             if isinstance(term, tuple(sympy.core.all_classes)):
                 term = sympy.simplify(term)
-            result.append(term)
+            if term != 0:
+                result[tuple(m)] = func.Function(term, dirs=self.dirs)
         assert rem == 0
-        result = []
-        nz =  [(m, r) for m, r in zip(mult, result) if r != 0]
-        for m, r in nz:
-            split_coeff = func.Function(nz[i], dirs=self.dirs).split()
-            tensorizable = len(split_coeff) == len(self.dirs) + 1
-            if tensorizable:
-                split_op = []
-                for i in range(dim):
-                    derivative = unknown.diff(*([variables[i]]*m[i]))
-                    split_op.append(split_coeff[i]*derivative)
-                split_op.append(split_coeff[-1])
+        return result
