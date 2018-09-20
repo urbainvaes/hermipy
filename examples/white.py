5a3a9a3240e43b2f27c4ef86b1a753f0dcc0c86a
Urbain Vaes <urbain@vaes.uk>
2018-09-20 12:40:47 +0200

    Improve white noise example

--- examples/white.py
+++ examples/white.py
@@ -1,4 +1,4 @@
-#1 Copyright (C) 2018 Urbain Vaes
+# Copyright (C) 2018 Urbain Vaes
 
 import os
 import argparse
@@ -9,11 +9,12 @@ import hermipy as hm
 import hermipy.equations as eq
 import matplotlib.pyplot as plt
 import scipy.io
+import scipy.integrate
 
 hm.settings['tensorize'] = True
 hm.settings['sparse'] = True
 hm.settings['trails'] = False
-hm.settings['cache'] = True
+hm.settings['cache'] = False
 
 # Process arguments
 parser = argparse.ArgumentParser()
@@ -24,6 +25,7 @@ parser.add_argument('-m0', '--mass0', type=str)
 parser.add_argument('-m', '--mass', type=str)
 parser.add_argument('-t', '--theta', type=str)
 parser.add_argument('-d', '--degree', type=int)
+parser.add_argument('--method', type=str)
 args = parser.parse_args()
 
 # Directory for output files
@@ -62,24 +64,14 @@ m, Vp = s('m', real=True), x**4/4 - x**2/2
 params = {'β': β, 'm': m, 'θ': θ, 'Vp': Vp}
 forward = equation.equation(params)
 
-# Load data
-data_file = 'bdata/sol_beta_{}_mean_{}.mat'.format(str(β), str(float(m0)))
-try:
-    data = scipy.io.loadmat(data_file)
-    time, rho, xs = data['t'].T[0][1:], data['p'], data['x'][0]
-except IOError:
-    exit(0)
-
-# Define quadrature for comparison
-quad_comparison = hm.Quad(nodes=[xs], weights=[xs*0], types=["visu"])
-
-sx, mx = r(1, 5), r(0)
+sx, mx = r(1, 10), r(0)
 degree = args.degree if args.degree else 100
 kwargs0 = {'degree': degree}
 n_points_num = 2*degree + 1
 
 # Potential for approximation
 Vq = sym.Rational(1/2)*x*x/sx
+# factor = sym.exp(-Vq/2 - β*Vp/2)
 factor = sym.exp(-Vq/2)
 
 # Calculation of the solution
@@ -98,11 +90,114 @@ r_operator = (forward - m*m_operator).cancel()
 r_mat = quad.discretize_op(r_operator, **kwargs0)
 m_mat = quad.discretize_op(m_operator, **kwargs0)
 
-# Initial condition
-u = sym.exp(-(x-m0)*(x-m0)/2)
-u = u / quad.integrate(u, flat=True)
-t = quad.transform(u, degree=degree)
-m = float(m1*t)
+if False:
+    degrees, eigvals = list(range(4, 50)), []
+    for d in degrees:
+        e, v = r_mat.subdegree(d).eigs(k=1, which='LR')
+        eigvals.append(e)
+        print(e)
+    fig, ax = plt.subplots()
+    ax.plot(degrees, eigvals, 'k.')
+    ax.set_yscale('symlog', linthreshy=1e-5)
+    ax.set_xlabel('Number of Hermite functions')
+    ax.set_ylabel('$\\lambda_0$')
+    plt.savefig('eigen.pdf', bbox_inches='tight')
+    plt.show()
+
+# Load data
+data_file = 'bdata/sol_beta_{}_mean_{}.mat'.format(str(β), str(float(m0)))
+try:
+    data = scipy.io.loadmat(data_file)
+    time, rho, xs = data['t'].T[0][1:], data['p'], data['x'][0]
+except IOError:
+    exit(0)
+
+subsample = 1
+time = [time[i] for i in range(0, len(time) - 1, subsample)]
+rho = [rho[i] for i in range(0, len(rho) - 1, subsample)]
+
+# Define quadrature for comparison
+quad_comparison = hm.Quad(nodes=[xs], weights=[xs*0], types=["visu"])
+
+
+def error_Linf(result):
+    errors = np.zeros(len(result))
+    for i, t in enumerate(result):
+        Δ = quad_comparison.eval(t) - rho[i]
+        errors[i] = np.sum(np.abs(Δ))*(xs[-1] - xs[0])/(len(Δ) - 1)
+    return np.max(errors)
+
+
+def solve(method, subdegree=degree):
+
+    # Subdegree
+    sub_r_mat = r_mat.subdegree(subdegree)
+    sub_m_mat = m_mat.subdegree(subdegree)
+    sub_In = In.subdegree(subdegree)
+    sub_m1 = m1.subdegree(subdegree)
+
+    # Initial condition
+    u = sym.exp(-(x-m0)*(x-m0)/2)
+    u = u / quad.integrate(u, flat=True)
+    t = quad.transform(u, degree=subdegree)
+    m = float(sub_m1*t)
+
+    if method == "ode45":
+
+        def dfdt(t, y):
+            m = np.dot(sub_m1.coeffs, y) / np.dot(sub_In.coeffs, y)
+            mat = sub_r_mat.matrix + m*sub_m_mat.matrix
+            return_vector = mat.dot(y)
+            return return_vector
+
+        result = scipy.integrate.solve_ivp(dfdt, [0, 5], t.coeffs, 'RK45',
+                                           t_eval=time, max_step=.01)
+        result = [quad.series(y) for y in result.y.T]
+
+    elif method == "semi_explicit":
+
+        eye = quad.varf('1', degree=subdegree)
+        steps = np.diff(time)
+        scheme = 'crank'
+        result = [t]
+
+        for i, dt in enumerate(steps):
+
+            print("i: {}, t: {}, m: {}".format(i, time[i], m))
+
+            mat = sub_r_mat + m*sub_m_mat
+            if scheme == 'backward':
+                total_op = eye - dt*mat
+                new_t = total_op.solve(t)
+
+            if scheme == 'crank':
+                crank_left = eye - dt*mat*(1/2)
+                crank_right = eye + dt*mat*(1/2)
+                new_t = crank_left.solve(crank_right(t))
+
+            # Normalization
+            m = float(sub_m1*new_t) / float(sub_In*new_t)
+
+            # t = new_t / float(In*new_t)
+            t = new_t
+            result.append(t)
+
+    return result
+
+
+degrees = list(range(10, degree))
+errors = np.zeros(len(degrees))
+for i, d in enumerate(degrees):
+    method = args.method if args.method else "ode45"
+    result = solve("ode45", subdegree=d)
+    errors[i] = error_Linf(result)
+    print("Linf((0, T), L1) Error: " + str(errors[i]))
+np.save("comparison_carrillo_degrees", degrees)
+np.save("comparison_carrillo_errors", errors)
+
+# Plots {{{
+if True:
+    exit(0)
 
 if args.interactive:
     plt.ion()
@@ -120,38 +215,19 @@ def plot(i, t, title=None):
 
     ax[1].clear()
     evalualion = quad_comparison.eval(t)
-    quad_comparison.plot(rho[i] - evalualion, ax=ax[1], bounds=False, title="Error")
+    quad_comparison.plot(rho[i] - evalualion, ax=ax[1],
+                         bounds=False, title="Error")
     # t.plot(ax=ax[1])
 
     plt.draw()
     plt.pause(0.01)
 
 
-eye = quad.varf('1', degree=degree)
-steps = np.diff(time)
-scheme = 'crank'
-
-for i, dt in enumerate(steps):
-
-    # Error
+for i, t in enumerate(result):
     Δ = quad_comparison.eval(t) - rho[i]
     Δ = np.sum(np.abs(Δ))*(xs[-1] - xs[0])/(len(Δ) - 1)
-
-    if args.interactive and i % 10 == 0:
+    print(Δ)
+    if args.interactive and i % 1 == 0:
         plot(i, t)
 
-    print("i: {}, t: {}, m: {}, Δ: {}".format(i, time[i], m, Δ))
-
-    mat = r_mat + m*m_mat
-    if scheme == 'backward':
-        total_op = eye - dt*mat
-        new_t = total_op.solve(t)
-
-    if scheme == 'crank':
-        crank_left = eye - dt*mat*(1/2)
-        crank_right = eye + dt*mat*(1/2)
-        new_t = crank_left.solve(crank_right(t))
-
-    # Normalization
-    m = float(m1*new_t)
-    t = new_t / float(In*new_t)
+# }}}
