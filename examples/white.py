b078bee7af961dfd8bc30edb2fc14e1f2c5c0e7e
Urbain Vaes <urbain@vaes.uk>
2019-07-08 10:48:39 +0200

    Save changes

--- examples/white.py
+++ examples/white.py
@@ -13,7 +13,7 @@ import matplotlib
 hm.settings['tensorize'] = True
 hm.settings['sparse'] = True
 hm.settings['trails'] = False
-hm.settings['cache'] = False
+hm.settings['cache'] = True
 
 # Process arguments
 parser = argparse.ArgumentParser()
@@ -47,7 +47,7 @@ if args.directory:
 matplotlib.rc('font', size=20)
 matplotlib.rc('font', family='serif')
 matplotlib.rc('text', usetex=True)
-matplotlib.rc('figure', figsize=(14, 8))
+# matplotlib.rc('figure', figsize=(14, 8))
 
 # Dimension of the problem
 dim = 1
@@ -71,16 +71,17 @@ def set_param(arg, default, symbol):
 m0 = set_param(args.mass0, r(0), 'm0')
 θ = set_param(args.theta, r(1, 1), 'θ')
 m, Vp = s('m', real=True), x**4/4 - x**2/2
+Vp = x**2/2
 params = {'β': β, 'm': m, 'θ': θ, 'Vp': Vp}
 forward = equation.equation(params)
 
-sx, mx = r(1, 10), r(0)
+sx, mx = r(1, 10), r(0.0)
 degree = args.degree if args.degree else 100
 kwargs0 = {'degree': degree}
 n_points_num = 2*degree + 1
 
 # Potential for approximation
-Vq = sym.Rational(1/2)*x*x/sx
+Vq = (sym.Rational(1/2))*(x-mx)*(x-mx)/sx
 # factor = sym.exp(-Vq/2 - β*Vp/2)
 factor = sym.exp(-Vq/2)
 
@@ -100,12 +101,28 @@ r_operator = (forward - m*m_operator).cancel()
 r_mat = quad.discretize_op(r_operator, **kwargs0)
 m_mat = quad.discretize_op(m_operator, **kwargs0)
 
+full_v = None
 if args.test_eigs:
     degrees, eigvals = list(range(4, degree)), []
     for d in degrees:
-        e, v = r_mat.subdegree(d).eigs(k=1, which='LR')
+        sub_r = r_mat.subdegree(d)
+        e, v = sub_r.eigs(k=1, which='LR')
         eigvals.append(e)
+        if e[0] < 0 and full_v is None:
+            full_v = (v[0]).subdegree(degree)
+
+        if full_v is not None:
+            print(full_v.subdegree(d)*sub_r(full_v.subdegree(d)))
         print(e)
+
+        # if e[0] < 0:
+        #     nv = (v[0]).subdegree(degree)
+        #     fig, ax = plt.subplots()
+        #     quad.plot(r_mat(nv), ax=ax)
+        #     quad.plot(sub_r(v[0]), ax=ax)
+        #     plt.show()
+        #     plt.close(fig)
+
     fig, ax = plt.subplots()
     ax.plot(degrees, eigvals, 'k.')
     ax.set_yscale('symlog', linthreshy=1e-5)
@@ -119,6 +136,10 @@ data_file = 'bdata/sol_beta_{}_mean_{}.mat'.format(str(β), str(float(m0)))
 try:
     data = scipy.io.loadmat(data_file)
     time, rho, xs = data['t'].T[0][1:], data['p'], data['x'][0]
+    # print((1/np.sqrt(2*np.pi))*np.exp(-(xs - .1)**2/2))
+    # plt.plot(xs, rho[0] - (1/np.sqrt(2*np.pi))*np.exp(-(xs - .1)**2/2))
+    rho[0] = (1/np.sqrt(2*np.pi))*np.exp(-(xs - .1)**2/2)
+    plt.show()
 except IOError:
     exit(0)
 
@@ -130,15 +151,7 @@ rho = [rho[i] for i in range(0, len(rho) - 1, subsample)]
 quad_comparison = hm.Quad(nodes=[xs], weights=[xs*0], types=["visu"])
 
 
-def error_Linf(result):
-    errors = np.zeros(len(result))
-    for i, t in enumerate(result):
-        Δ = quad_comparison.eval(t) - rho[i]
-        errors[i] = np.sum(np.abs(Δ))*(xs[-1] - xs[0])/(len(Δ) - 1)
-    return np.max(errors)
-
-
-def solve(method, subdegree=degree):
+def solve(method, subdegree=degree, limit=False):
 
     # Subdegree
     sub_r_mat = r_mat.subdegree(subdegree)
@@ -149,7 +162,12 @@ def solve(method, subdegree=degree):
     # Initial condition
     u = sym.exp(-(x-m0)*(x-m0)/2)
     u = u / quad.integrate(u, flat=True)
-    t = quad.transform(u, degree=subdegree)
+
+    if limit:
+        t = quad.transform(u, degree=25)
+        t = t.subdegree(degree=subdegree)
+    else:
+        t = quad.transform(u, degree=subdegree)
     m = float(sub_m1*t)
 
     if method == "ode45":
@@ -161,7 +179,7 @@ def solve(method, subdegree=degree):
             return return_vector
         result = scipy.integrate.solve_ivp(dfdt, [0, 5], t.coeffs, 'RK45',
                                            t_eval=time, max_step=.01,
-                                           atol=1e-9, rtol=1e-9)
+                                           atol=1e-11, rtol=1e-11)
         result = [quad.series(y) for y in result.y.T]
 
     elif method == "semi_explicit":
@@ -195,18 +213,39 @@ def solve(method, subdegree=degree):
     return result
 
 
+method = args.method if args.method else "ode45"
+exact = solve(method, subdegree=degree)
+exact_eval = [quad_comparison.eval(e) for e in exact]
+error_rho = [e - rho[i] for i, e in enumerate(exact_eval)]
+error_rho = [np.sum(np.abs(e)) * (xs[-1] - xs[0])/(len(e) - 1)
+             for e in error_rho]
+error_rho = np.max(error_rho)
+# exact_eval = rho
+
+
+def error_Linf(result):
+    errors = np.zeros(len(result))
+    for i, t in enumerate(result):
+        Δ = quad_comparison.eval(t) - exact_eval[i]
+        errors[i] = np.sum(np.abs(Δ))*(xs[-1] - xs[0])/(len(Δ) - 1)
+    return np.max(errors)
+
+
 if args.test_convergence:
 
     try:
-        degrees = np.load("data/comparison_carillo/comparison_carrillo_degrees_ode45_80.npy")
-        errors_ode45 = np.load("data/comparison_carillo/comparison_carrillo_errors_ode45_80.npy")
-        errors_semi_explicit = np.load("data/comparison_carillo/comparison_carrillo_errors_semi_explicit_80.npy")
+        degrees = np.load("comparison_carrillo_degrees_ode45.npy")
+        errors_ode45 = np.load("comparison_carrillo_errors_ode45.npy")
+        errors_semi_explicit = np.load("comparison_carrillo_errors_semi_explicit.npy")
 
-        fig, ax = plt.subplots()
-        plt.semilogy(degrees, errors_ode45, '.', label='RK45')
-        plt.semilogy(degrees, errors_semi_explicit, '.', label='Semi-implicit')
+        fig = plt.figure(figsize=(8, 6))
+        ax = plt.gca()
+        plt.semilogy(degrees, errors_ode45, '.', label='RK45 (spectral)')
+        degrees = np.load("comparison_carrillo_degrees_semi_explicit.npy")
+        plt.semilogy(degrees, errors_semi_explicit, '.', label='Semi-implicit (spectral)')
+        plt.semilogy(degrees, degrees*0 + error_rho, '-', label='Finite volume')
         ax.set_xlabel("Number of Hermite functions")
-        ax.set_title("Difference (in the $L^\\infty(0, T; L^1(\\mathbf R))$ norm)")
+        ax.set_title("Error (in the $L^\\infty(0, T; L^1(\\mathbf R))$ norm)")
         plt.legend()
         plt.savefig('comparison-degree-errors.pdf', bbox_inches='tight')
         plt.show()
@@ -214,36 +253,62 @@ if args.test_convergence:
     except IOError:
         pass
 
-    degrees = list(range(80, degree))
+    degrees = list(range(10, 80))
     errors = np.zeros(len(degrees))
     method = args.method if args.method else "ode45"
 
     for i, d in enumerate(degrees):
         result = solve(method, subdegree=d)
         errors[i] = error_Linf(result)
+        print(errors[i])
     np.save("comparison_carrillo_degrees_{}".format(method), degrees)
     np.save("comparison_carrillo_errors_{}".format(method), errors)
 
 # Plots {{{1
 if args.test_plots:
 
-    result = solve('ode45')
+    # result1 = solve('ode45')
+    result_exact = solve('ode45')
+    result_coarse = solve('ode45', subdegree=25)
+    result_limited = solve('ode45', limit=25)
+
+    error_coarse, error_limited, delta_initial = [], [], []
+
+    def L1(function):
+        return np.sum(np.abs(function))*(xs[-1] - xs[0])/(len(function) - 1)
+
+
+    for i, r in enumerate(result_exact):
+        error_coarse.append(L1(quad_comparison.eval(r) - quad_comparison.eval(result_coarse[i])))
+        error_limited.append(L1(quad_comparison.eval(r) - quad_comparison.eval(result_limited[i])))
+        delta_initial.append(L1(quad_comparison.eval(result_coarse[i]) - quad_comparison.eval(result_limited[i])))
+
+    plt.plot(time, error_coarse, label='Error coarse')
+    plt.plot(time, error_limited, label='Error limited')
+    plt.plot(time, delta_initial, label='Error initial')
+    plt.legend()
+    plt.show()
+    import ipdb; ipdb.set_trace()
+
 
     times = [0, 1, 2, 5]
     for t in times:
         i = np.argmin(np.abs(np.asarray(time) - t))
         fig, ax = plt.subplots()
         ax.set_xlim(-4, 4)
-        quad_comparison.plot(result[i], ax=ax, bounds=False,
-                             label="Spectral")
-        quad_comparison.plot(rho[i], ax=ax, bounds=False,
-                             label="Finite-volume",
-                             title="Time: {0:.2f}".format(time[i]))
+        Δ = quad_comparison.eval(result1[i]) - result2[i]
+        error = np.sum(np.abs(Δ))*(xs[-1] - xs[0])/(len(Δ) - 1)
+        quad_comparison.plot(result1[i], ax=ax, bounds=False,
+                             label="Spectral method ($d = 25$)")
+        quad_comparison.plot(result2[i], ax=ax, bounds=False,
+                             label="Spectral method ($d = 100$)",
+                             title="Time: {:.2f}, $L^1(\\mathbf R)$ difference: {:.2e}".format(time[i], error))
         ax.set_xlabel('$x$')
         if t == 5:
             plt.legend(loc='lower left')
         plt.savefig('comparison_carrillo_solution-{}.pdf'.format(t),
                     bbox_inches='tight')
+        # result1[i].plot()
         plt.show()
 
     plt.ion()
